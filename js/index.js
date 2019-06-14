3b3fd0f10b6bb608c02cf5c3fe65a259c6954ec9
didadi599 <1041288269@qq.com>
2019-06-14 10:58:38 +0800

    updata

--- js/index.js
+++ js/index.js
@@ -1,24 +1,64 @@
-function editor () {
+function mdConfig () {
   var md = window.markdownit({
     typographer: true,
 
     highlight: function (str, lang) {
       if (lang && hljs.getLanguage(lang)) {
         try {
-          return '<pre class="hljs"><code>' +
+          return '<pre class="hljs">' +
             hljs.highlight(lang, str, true).value +
-            '</code></pre>';
+            '</pre>';
         } catch (__) {
           return __
         }
       }
 
-      return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+      return '<pre class="hljs">' + md.utils.escapeHtml(str) + '</pre>';
     }
   });
 
+  md.use(window.markdownitContainer, 'tips', {
+    render: function (tokens, idx) {
+      var m = tokens[idx].info.trim().match(/^tips(.*)$/);
+
+      if (tokens[idx].nesting === 1) {
+        return '<section class="tips"><p class="tips-title">Tip</p>' + md.utils.escapeHtml(m[1]);
+      } else {
+        return '</section>\n';
+      }
+    }
+  });
+
+  md.use(window.markdownitEmoji);
+
+  md.use(window.markdownitSup);
+
+  md.use(window.markdownitSub);
+
+  md.use(window.markdownitIns);
+
+  md.use(window.markdownitMark);
+
+  md.use(window.markdownitFootnote);
+
+  md.use(window.markdownitDeflist);
+
+  md.use(window.markdownitAbbr);
+
+  console.log(md)
+
+  return md;
+}
+
+function editor () {
+  var md = mdConfig();
+
   var textareaEle = document.getElementById('textarea');
   var paperEle = document.getElementById('paper');
+
+  var mdHtml = md.render(textareaEle.value);
+
+  paperEle.innerHTML = mdHtml;
 
   textareaEle.onkeydown = function (e) {
     var value = md.render(e.target.value);
